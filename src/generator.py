01f3de08a4748ef26c070983a22193d7072e1c16
Colin Bush <colin.bush@slu.edu>
2023-09-11 17:35:08 -0500

    removed unnecessary variable

--- src/generator.py
+++ src/generator.py
@@ -2,8 +2,7 @@ from maze import Maze
 import sys
 
 try:
-    maze_size = int(sys.argv[1])
-    maze = Maze(maze_size)
+    maze = Maze(int(sys.argv[1])) # Uses command line argument to generate maze with given size
     maze.generate_maze()
     maze.print()
     print("Welcome to 2D maze")
